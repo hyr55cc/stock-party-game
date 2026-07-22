06de111d0428dbae7babe61ae0176de93b6ceeae
hith <ha.stored5@gmail.com>
2026-07-22 17:46:08 +0300

    إصلاح: رابط ومفتاح Supabase بالمكان الصحيح

--- public/supabase-config.js
+++ public/supabase-config.js
@@ -3,7 +3,7 @@
 //  Project Settings > API > Project URL / anon public key
 //  هاتين القيمتين آمنتين للاستخدام بواجهة العميل (ليستا سرّيتين)
 // ============================================================
-const SUPABASE_URL = 'Ysb_publishable_MfmoQQvnZ4-K_L-Q5GqIcw_5tbzlb94';
+const SUPABASE_URL = 'https://rptgylrhmzthnzgnjccn.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwdGd5bHJobXp0aG56Z25qY2NuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2OTAxMDIsImV4cCI6MjEwMDI2NjEwMn0.HywtByDbxIR9JT210unvn0wUdglJlBRWooM09t0QaXQ';
 
 const supabaseClient = (SUPABASE_URL.startsWith('http'))
